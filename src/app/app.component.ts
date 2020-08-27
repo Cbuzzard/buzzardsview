dfddaa097942e196607b675de8a2a4d2196ce5f9
cbuzzard <corwin1998@gmail.com>
2020-08-26 21:49:29 -0400

    bug fixes in mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,39 +13,43 @@ export class AppComponent {
 
   calculateTranslation(el) {
     this.dimmer.nativeElement.style.zindex=5;
+    let windowWidthCenter = window.innerWidth/2;
+    let windowHeightCenter = window.innerHeight/2;
     if (this.elementCurrentlyExpanded) {
       this.dimmer.nativeElement.style.zindex= 1;
       this.elementCurrentlyExpanded.style.pointerEvents= 'none';
       this.dimmer.nativeElement.style.opacity = 0;
+      this.elementCurrentlyExpanded.style.transition = 'transform .4s ease-in-out';
       this.elementCurrentlyExpanded.style.transform = ``;
       setTimeout(()=>{
+        this.elementCurrentlyExpanded.style.transition = 'transform .2s ease-in-out';
         this.elementCurrentlyExpanded.style.pointerEvents= '';
         this.elementCurrentlyExpanded = null;
         this.dimmer.nativeElement.style.visibility = 'hidden';
       }, 400)
-      
-      
-      
     } else {
       el = el.el.nativeElement;
-
+      console.log(el);
       let elDimensions = el.getBoundingClientRect();
       let windowWidthCenter = window.innerWidth/2;
       let windowHeightCenter = window.innerHeight/2;
-
       let x = (windowWidthCenter - elDimensions.left) - (elDimensions.width/2);
       let y = (windowHeightCenter - elDimensions.top) - (elDimensions.height/2);
-      el.style.transform = `translate(${x}px, ${y}px) rotateY(180deg) scale(2)`;
+      el.style.transition = 'transform .4s ease-in-out';
+      if(windowWidthCenter <= 385) {
+        let logo = el.querySelectory('.logo');
+        logo.style.transform = 'scaleY(-2)';
+        el.style.transform = `translate(${x}px, ${y}px) rotateY(180deg) scaleY(2)`;   
+      } else {
+        el.style.transform = `translate(${x}px, ${y}px) rotateY(180deg) scale(2)`;
+      }
+      setTimeout(() => {
+        el.style.transition = 'transform .2s ease-in-out'
+      }, 400);
       el.style.zindex=6;
-
       this.dimmer.nativeElement.style.visibility = 'visible';
       this.dimmer.nativeElement.style.opacity = 1;
-
-
       this.elementCurrentlyExpanded = el;
-  
     }
-    
   }
-
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -11,49 +11,11 @@ import {
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css'],
-  animations: [
-    trigger('openClose', [
-      state('closed', style({
-        transform: 'translate(0, 0)'
-      })),
-      state('open', style({
-        transform: `translate(50px, 50px)`
-      })),
-      transition('open => closed', [
-        animate('1s')
-      ]),
-      transition('closed => open', [
-        animate('1s')
-      ]),
-    ])
-  ],
 })
 export class CardComponent implements OnInit {
-
-  isOpen = false;
-
-  x = 20;
 
   constructor(private el: ElementRef) { }
 
   ngOnInit(): void {
-    console.log(this.el.nativeElement.getBoundingClientRect());
   }
-
-  toggle() {
-    this.isOpen = !this.isOpen;
-  }
-
-  calculateTranslation() {
-    let elDimensions = this.el.nativeElement.getBoundingClientRect();
-    let windowWidthCenter = window.innerWidth/2;
-    let windowHeightCenter = window.innerHeight/2;
-
-    let x = (windowWidthCenter - elDimensions.left) - (elDimensions.width/2);
-    let y = (windowHeightCenter - elDimensions.top) - (elDimensions.height/2);
-
-    return `${x}px ${y}px`
-  }
-
-
 }
