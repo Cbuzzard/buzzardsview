622540b6bc4c2ba358973816d5e924936d295803
cbuzzard <corwin1998@gmail.com>
2020-09-01 18:21:01 -0400

    finished flip functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,24 +12,29 @@ export class AppComponent {
   @ViewChild('dimmer') dimmer;
 
   calculateTranslation(el) {
-    this.dimmer.nativeElement.style.zindex=5;
+    this.dimmer.nativeElement.style.zIndex=5;
     let windowWidthCenter = window.innerWidth/2;
     let windowHeightCenter = window.innerHeight/2;
+
     if (this.elementCurrentlyExpanded) {
-      this.dimmer.nativeElement.style.zindex= 1;
-      this.elementCurrentlyExpanded.style.pointerEvents= 'none';
+      this.elementCurrentlyExpanded.classList.remove('hover')
       this.dimmer.nativeElement.style.opacity = 0;
       this.elementCurrentlyExpanded.style.transition = 'transform .4s ease-in-out';
-      this.elementCurrentlyExpanded.style.transform = ``;
+      this.elementCurrentlyExpanded.style.transform = null;
+      this.elementCurrentlyExpanded.querySelector('.back').style.transform = null;
+      this.elementCurrentlyExpanded.querySelector('.logocontainer').style.transform = null
       setTimeout(()=>{
+        this.elementCurrentlyExpanded.style.zIndex = null;
+        this.dimmer.nativeElement.style.zIndex= null;
         this.elementCurrentlyExpanded.style.transition = 'transform .2s ease-in-out';
-        this.elementCurrentlyExpanded.style.pointerEvents= '';
+        this.elementCurrentlyExpanded.classList.add('hover')
         this.elementCurrentlyExpanded = null;
         this.dimmer.nativeElement.style.visibility = 'hidden';
       }, 400)
     } else {
       el = el.el.nativeElement;
-      console.log(el);
+      el.querySelector('.back').style.transform = 'rotateY(180deg)'
+      el.querySelector('.logocontainer').style.transform = 'rotateY(180deg)'
       let elDimensions = el.getBoundingClientRect();
       let windowWidthCenter = window.innerWidth/2;
       let windowHeightCenter = window.innerHeight/2;
@@ -37,16 +42,14 @@ export class AppComponent {
       let y = (windowHeightCenter - elDimensions.top) - (elDimensions.height/2);
       el.style.transition = 'transform .4s ease-in-out';
       if(windowWidthCenter <= 385) {
-        let logo = el.querySelectory('.logo');
-        logo.style.transform = 'scaleY(-2)';
-        el.style.transform = `translate(${x}px, ${y}px) rotateY(180deg) scaleY(2)`;   
+        el.style.transform = `translate(${x}px, ${y}px) rotateY(180deg) scale(1.2)`;   
       } else {
         el.style.transform = `translate(${x}px, ${y}px) rotateY(180deg) scale(2)`;
       }
       setTimeout(() => {
         el.style.transition = 'transform .2s ease-in-out'
       }, 400);
-      el.style.zindex=6;
+      el.style.zIndex=6;
       this.dimmer.nativeElement.style.visibility = 'visible';
       this.dimmer.nativeElement.style.opacity = 1;
       this.elementCurrentlyExpanded = el;
